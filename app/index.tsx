4f11a04921b35305b3ae825632a1eb0cf384b11e
Rafi Ferdos <rafiferdos@gmail.com>
2026-02-20 14:02:35 +0600

    remove unused components and files; add new index screen with basic layout

---
+++ app/index.tsx
@@ -0,0 +1,26 @@
+import { Button, Image, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+
+export default function Index() {
+  return (
+    <ScrollView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text style={styles.fontColor}>Edit app/index.tsx to edit this screen rafi.</Text>
+      <Button title={'Wanna Click Me!?'} />
+        <TextInput placeholder='type here bro' />
+        <Image source={require('@/assets/images/icon.png')} />
+    </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  fontColor: {
+    color: 'red',
+  }
+})
