4f86712028c0bffcc4bdabbb15ab006c3bf281d9
Rafi Ferdos <rafiferdos@gmail.com>
2026-02-20 15:44:53 +0600

    refactor index screen layout for improved readability and consistency

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,36 @@
-import { Button, Image, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native'
 
 export default function Index() {
   return (
     <ScrollView>
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text style={styles.fontColor}>Edit app/index.tsx to edit this screen rafi.</Text>
-      <Button title={'Wanna Click Me!?'} />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={styles.fontColor}>
+          Edit app/index.tsx to edit this screen rafi.
+        </Text>
+        <Button title={'Wanna Click Me!?'} />
         <TextInput placeholder='type here bro' />
-        <Image source={require('@/assets/images/icon.png')} />
-    </View>
+        <Image
+          style={{
+            height: 100,
+            width: 100,
+          }}
+          source={require('@/assets/images/icon.png')}
+        />
+      </View>
     </ScrollView>
   )
 }
@@ -22,5 +38,5 @@ export default function Index() {
 const styles = StyleSheet.create({
   fontColor: {
     color: 'red',
-  }
+  },
 })
